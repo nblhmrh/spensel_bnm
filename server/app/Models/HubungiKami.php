84723737cf0aeb36cc4f44c236b28dfeed548914
nblhmrh <nabilahhumairah@gmail.com>
2025-04-23 23:43:04 +0800

    be

--- server/app/Models/HubungiKami.php
+++ server/app/Models/HubungiKami.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HubungiKami extends Model
+class Hubungikami extends Model
 {
     use HasFactory;
 
