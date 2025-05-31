5f614c28ca389538c5e261ead6f9f86bec770363
MuhAsshiddiqieAmin <dikimks288@gmail.com>
2025-05-31 20:29:29 +0800

    perbaikan

--- client/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["source.unsplash.com"], // Izinkan gambar dari Unsplash
-  },
-};
-
-module.exports = nextConfig;
-
